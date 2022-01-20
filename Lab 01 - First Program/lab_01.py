f1e776d458573dc9e9ca1bde38dbaed3c8781581
littlettob <90487368+littlettob@users.noreply.github.com>
2022-01-19 18:11:26 -0700

    Update lab_01.py

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -2,6 +2,6 @@ print("Hello Everyone \r\n\tMy Name is Thien")
 print("Python Programming")
 print("This is my \rfirst Assignment.")
 print("This\nis\nso\ncool.")
-print("""This
-is
-triple""")
+print("""\tThis
+\tis
+\ttriple""")
