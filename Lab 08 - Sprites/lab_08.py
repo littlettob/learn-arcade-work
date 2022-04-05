800ede32c0eeffaaed2f0cfa2396bd6d2f53ccd0
littlettob <90487368+littlettob@users.noreply.github.com>
2022-04-05 03:05:44 -0600

    Add files via upload

--- Lab 08 - Sprites/lab_08.py
+++ Lab 08 - Sprites/lab_08.py
@@ -1,4 +1,3 @@
-
 import arcade
 import random
 
@@ -20,47 +19,40 @@ class MyGame(arcade.Window):
         # backgound
         arcade.set_background_color(arcade.color.BLACK)
         # add Character Image
-        self.character= arcade.Sprite("spaceship.png", 1.5 ,center_x=50, center_y=50)
-        self.charecter_list= arcade.SpriteList()
-        self.charecter_list.append(self.character)
+        self.character_list = arcade.SpriteList()
+        self.character = arcade.Sprite("spaceship.png", 1.5)
+        self.character_list.append(self.character)
+
+
         # Backgound atmosphere sound
         self.audio_name = arcade.sound.load_sound("atmosphere.mp3")
-    """"    #create a explosion
-        self.explosion_list = arcade.SpriteList()
-        self.explosion_texture_list=[]
-        for i in range (40,80):
-            texture_name = f"explosion{i:3}.png"
-            texture= arcade.load.texture(texture_name)
-            self.explosion_texure_list.append(texture)"""
 
-        #create a star
+        # create a star
+
         self.star_list = arcade.SpriteList()
-    for i in range (100):
-                star = arcade.sprite("star.png",1.2)
-                star.center_x = random.randit(1,800)
-                star.center_y = random.randit(1,400)
-        self.star_list.append(star)
-
-
-
+        for i in range (100):
+            star = arcade.Sprite("star.png")
+            star.center_x=random.randint(0,600)
+            star.center_y = random.randint(0, 600)
+            self.star_list.append(star)
 
 
     def on_draw(self):
         #draw the window.
         arcade.start_render()
-        self.charecter_list.draw()
+        self.character_list.draw()
         self.star_list.draw()
         #play a sound effect
         arcade.sound.play_sound(self.audio_name)
 
     def on_update(self, delta_time):
-        self.charecter_list.update()
-        hit_list = arcade.check.for_collision_with_list(self.charecter_list,self.star_list)
-        # Loop for hit list
+        self.character_list.update()
+
+        hit_list = arcade.check_for_collision_with_list(self.character,
+                                                             self.star_list)
+        # loop through each colliding sprite and remove it
         for star in hit_list:
             star.remove_from_sprite_lists()
-
-
 
     def on_mouse_motion(self, x, y, dx, dy):
         self.character.center_x = x
